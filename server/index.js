2f42fd2bb4ff0e3cb89b5abbdb7dda5f2a468639
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-30 19:14:50 -0300

    tratamento de erros com o try catch em nosso database schema

--- server/index.js
+++ server/index.js
@@ -7,21 +7,25 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.get('/',async (req, res) => {
-    const clients =  new Clients({
+app.get('/', async (req, res) => {
+    const clients = new Clients({
         name: 'Bene',
-        NumeroConta: "2",
+        NumeroConta: "5",
         dataNascimento: '1999-12-31',
         valor: 1000,
         dataEmissao: '2020-12-31',
         dataVencimento: '2021-12-31',
         cnpj: 123456789,
-        razaoSocial: 'BeneTesla',
-        nomeFantasia: 'BeneTesla'
-        
+        razaoSocial: 'Bene',
+        nomeFantasia: 'Bene'
     });
-    const result = await clients.save();
-    res.send(result);
+    try{
+        const result = await clients.save();
+        res.send(result);
+    } catch(e) {
+        res.send(e.message);
+    }
+    
 });
 mongoose.connect('mongodb+srv://bene:369369@cluster0.k9q6vh5.mongodb.net/?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: false }).then(() => {
