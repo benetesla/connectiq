3d58c1b488704411de769ba68339be925fd07ecc
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-30 17:41:38 -0300

    Conexao com o banco de dados estabeleciada

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 2000;
+app.use(express.json());
+
+//with this line we can use the .env file
+mongoose.connect('mongodb+srv://bene:369369@cluster0.k9q6vh5.mongodb.net/?retryWrites=true&w=majority',
+ { useNewUrlParser: true, useUnifiedTopology: false }).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Error connecting to MongoDB', err);
+});
+
+
+
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
