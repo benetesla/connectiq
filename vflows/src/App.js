2eb0b94305d3ba8401d35fb66a98b82fe382624d
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-02 10:13:07 -0300

    Removendo a navbar do sistema

--- vflows/src/App.js
+++ vflows/src/App.js
@@ -21,39 +21,7 @@ function App() {
   }
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img src={LOGO} className='LOGOTIPO' alt="logo" />
-          </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  <i className="fas fa-home"></i>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/contratos">
-                  <i className="fas fa-file-invoice-dollar"></i>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/dadosNF">
-                  <i className="fas fa-file-invoice"></i>
-                </a>
-              </li>
-              <li className='nav-item'>
-                <button className="btn btn-dark" onClick={DarKmode}> <i className="fas fa-moon"></i></button>
-              </li>
-            </ul>
-
-          </div>
-        </div>
-      </nav>
+      
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Form />} />
