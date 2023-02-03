aaecf9c8e27df196f3b4104181e8e82b7500b82e
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-03 18:47:06 -0300

    System

--- vflows/src/pages/DadosNF.js
+++ vflows/src/pages/DadosNF.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './DadosNF.css';
-const DadosNF = () => {
-    return (
-        <div className='card'>
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import { Users } from '../components/users';
+import LOGO from '../img/logo.png';
+import { ContratosEM } from '../components/ContratosEM';
 
-            {/**voltar para a pagina de acessar */}
-        </div>
-    );
+
+import './DadosNF.css';
+function contratos() {
+  return (
+    <div className='container'>
+      <img src={LOGO} class="rounded float-left " alt="..."></img>
+      <h2 className='text-center position-absolute'>PAGAMENTO DE FORNECEDOR</h2>
+     {/* <Users /> */}
+    </div>
+
+  )
 }
 
-export default DadosNF;
+export default contratos
